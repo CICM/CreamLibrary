f201156b8bd9472c01f5d55280d7dc371fb53ed7
pierreguillot <guillotpierre6@gmail.com>
2015-04-01 17:56:24 +0200

    upedateu

--- Chocolate/c.blackboard.cpp
+++ Chocolate/c.blackboard.cpp
@@ -178,7 +178,7 @@ void *blackboard_new(t_symbol *s, int argc, t_atom *argv)
     x->f_color      = gensym("#000000");
     x->f_fill       = 0;
     x->f_ninstructions = 0;
-    x->f_instructions = (char **)malloc(MAXPDSTRING * sizeof(char*));
+    x->f_instructions = (char **)malloc(MAX_COMMANDS * sizeof(char*));
     for(int i = 0; i < MAX_COMMANDS; i++)
     {
         x->f_instructions[i] = (char *)malloc(MAXPDSTRING * sizeof(char));
