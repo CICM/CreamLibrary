50c7a9117e701e0059bec9a334a5a883fb0f7089
pierreguillot <guillotpierre6@gmail.com>
2015-06-06 14:01:32 +0200

    Update prepend

--- Coffee/c.prepend.cpp
+++ Coffee/c.prepend.cpp
@@ -30,7 +30,7 @@ typedef struct  _prepend
 {
 	t_eobj      j_box;
 	t_outlet*   f_out;
-    t_atom      f_argv[256];
+    t_atom*     f_argv;
 	long        f_argc;
     t_symbol*   f_selector;
 
@@ -38,17 +38,178 @@ typedef struct  _prepend
 
 t_eclass *prepend_class;
 
-void *prepend_new(t_symbol *s, int argc, t_atom *argv);
-void prepend_anything(t_prepend *x, t_symbol *s, int argc, t_atom *argv);
-void prepend_list(t_prepend *x, t_symbol *s, int argc, t_atom *argv);
-void prepend_set(t_prepend *x, t_symbol *s, int argc, t_atom *argv);
-void prepend_float(t_prepend *x, float f);
-void prepend_symbol(t_prepend *x, t_symbol *s);
-void prepend_output(t_prepend *x, long size);
-void prepend_free(t_prepend *x);
-void prepend_assist(t_prepend *x, void *b, long m, long a, char *s);
+static void prepend_bang(t_prepend *x)
+{
+    t_atom* av = (t_atom *)malloc(1 + x->f_argc * sizeof(t_atom));
+    if(av)
+    {
+        memcpy(av, x->f_argv, x->f_argc * sizeof(t_atom));
+        atom_setsym(av+x->f_argc, &s_bang);
+        if(x->f_selector != &s_list)
+        {
+            outlet_anything(x->f_out, x->f_selector, 1 + x->f_argc, av);
+        }
+        else
+        {
+            outlet_list(x->f_out, &s_list, 1 + x->f_argc, av);
+        }
+        free(av);
+    }
+    else
+    {
+        pd_error(x, "can't allocate memory.");
+    }
+}
 
-void prepend_bang(t_prepend *x);
+static void prepend_float(t_prepend *x, float f)
+{
+    t_atom* av = (t_atom *)malloc(1 + x->f_argc * sizeof(t_atom));
+    if(av)
+    {
+        memcpy(av, x->f_argv, x->f_argc * sizeof(t_atom));
+        atom_setfloat(av+x->f_argc, f);
+        if(x->f_selector != &s_list)
+        {
+            outlet_anything(x->f_out, x->f_selector, 1 + x->f_argc, av);
+        }
+        else
+        {
+            outlet_list(x->f_out, &s_list, 1 + x->f_argc, av);
+        }
+        free(av);
+    }
+    else
+    {
+        pd_error(x, "can't allocate memory.");
+    }
+}
+
+static void prepend_symbol(t_prepend *x, t_symbol *s)
+{
+    t_atom* av = (t_atom *)malloc(1 + x->f_argc * sizeof(t_atom));
+    if(av)
+    {
+        memcpy(av, x->f_argv, x->f_argc * sizeof(t_atom));
+        atom_setsym(av+x->f_argc, s);
+        if(x->f_selector != &s_list)
+        {
+            outlet_anything(x->f_out, x->f_selector, 1 + x->f_argc, av);
+        }
+        else
+        {
+            outlet_list(x->f_out, &s_list, 1 + x->f_argc, av);
+        }
+        free(av);
+    }
+    else
+    {
+        pd_error(x, "can't allocate memory.");
+    }
+}
+
+static void prepend_list(t_prepend *x, t_symbol *s, int argc, t_atom *argv)
+{
+    if(argc && argv)
+    {
+        t_atom* av = (t_atom *)malloc(argc + x->f_argc * sizeof(t_atom));
+        if(av)
+        {
+            memcpy(av, x->f_argv, x->f_argc * sizeof(t_atom));
+            memcpy(av+x->f_argc, argv, argc * sizeof(t_atom));
+            if(x->f_selector != &s_list)
+            {
+                outlet_anything(x->f_out, x->f_selector, argc + x->f_argc, av);
+            }
+            else
+            {
+                outlet_list(x->f_out, &s_list, argc + x->f_argc, av);
+            }
+            free(av);
+        }
+        else
+        {
+            pd_error(x, "can't allocate memory.");
+        }
+    }
+    else
+    {
+        if(x->f_selector != &s_list)
+        {
+            outlet_anything(x->f_out, x->f_selector, x->f_argc, x->f_argv);
+        }
+        else
+        {
+            outlet_list(x->f_out, &s_list, x->f_argc, x->f_argv);
+        }
+    }
+}
+
+static void prepend_set(t_prepend *x, t_symbol *s, int argc, t_atom *argv)
+{
+    if(argc && argv)
+    {
+        if(atom_gettype(argv) == A_SYM)
+        {
+            x->f_argc = --argc;
+            x->f_selector = atom_getsym(argv++);
+        }
+        else
+        {
+            x->f_argc = argc;
+            x->f_selector = &s_list;
+        }
+        if(x->f_argv)
+        {
+            x->f_argv = (t_atom *)realloc(x->f_argv, x->f_argc * sizeof(t_atom));
+        }
+        else
+        {
+            x->f_argv = (t_atom *)malloc(x->f_argc * sizeof(t_atom));
+        }
+        if(x->f_argv)
+        {
+            for(int i = 0; i < x->f_argc; i++)
+            {
+                x->f_argv[i] = argv[i];
+            }
+        }
+        else
+        {
+            pd_error(x, "can't allocate memory.");
+        }
+    }
+    else
+    {
+        x->f_argc = 0;
+        if(x->f_argv)
+        {
+            free(x->f_argv);
+            x->f_argv = NULL;
+        }
+    }
+}
+
+static void prepend_free(t_prepend *x)
+{
+    if(x->f_argv)
+    {
+        free(x->f_argv);
+    }
+    eobj_free(x);
+}
+
+static void *prepend_new(t_symbol *s, int argc, t_atom *argv)
+{
+    t_prepend *x = (t_prepend *)eobj_new(prepend_class);
+    if(x)
+    {
+        x->f_argv = NULL;
+        x->f_argc = 0;
+        prepend_set(x, gensym("set"), argc, argv);
+        x->f_out = (t_outlet *)listout(x);
+    }
+    return (x);
+}
 
 extern "C" void setup_c0x2eprepend(void)
 {
@@ -57,7 +218,7 @@ extern "C" void setup_c0x2eprepend(void)
 	c = eclass_new("c.prepend", (method)prepend_new, (method)prepend_free, (short)sizeof(t_prepend), 0L, A_GIMME, 0);
     cream_initclass(c);
     
-    eclass_addmethod(c, (method)prepend_anything,    "anything",       A_GIMME, 0);
+    eclass_addmethod(c, (method)prepend_list,        "anything",       A_GIMME, 0);
     eclass_addmethod(c, (method)prepend_list,        "list",           A_GIMME, 0);
     eclass_addmethod(c, (method)prepend_set,         "set",            A_GIMME, 0);
     eclass_addmethod(c, (method)prepend_float,       "float",          A_FLOAT, 0);
@@ -68,150 +229,5 @@ extern "C" void setup_c0x2eprepend(void)
 	prepend_class = c;
 }
 
-void *prepend_new(t_symbol *s, int argc, t_atom *argv)
-{
-	t_prepend *x =  NULL;
-    
-    x = (t_prepend *)eobj_new(prepend_class);
-    if(x)
-    {
-        prepend_set(x, gensym("set"), argc, argv);
-        x->f_out = (t_outlet *)listout(x);
-    }
-    
-    return (x);
-}
-
-void prepend_set(t_prepend *x, t_symbol *s, int argc, t_atom *argv)
-{
-    int i;
-    if(argc && argv)
-    {
-        if(atom_gettype(argv) == A_SYM)
-        {
-            x->f_argc = argc - 1;
-            x->f_selector = atom_getsym(argv);
-            for(i = 0; i < x->f_argc; i++)
-            {
-                x->f_argv[i] = argv[i+1];
-            }
-        }
-        else
-        {
-            x->f_argc = argc;
-            x->f_selector = &s_list;
-            for(i = 0; i < x->f_argc; i++)
-            {
-                x->f_argv[i] = argv[i];
-            }
-        }
-        
-    }
-    else
-    {
-        x->f_argc = 0;
-    }
-}
-
-void prepend_list(t_prepend *x, t_symbol *s, int argc, t_atom *argv)
-{
-    int i;
-    if(x->f_argc || x->f_selector != &s_list)
-    {
-        if(x->f_argc + argc > 256)
-        {
-            argc = 256 - x->f_argc;
-        }
-        for(i = x->f_argc; i < x->f_argc + argc; i++)
-        {
-            x->f_argv[i] = argv[i - x->f_argc];
-        }
-        outlet_anything(x->f_out, x->f_selector, x->f_argc + argc, x->f_argv);
-    }
-    else if(x->f_selector != &s_list)
-    {
-        outlet_anything(x->f_out, x->f_selector, argc, argv);
-    }
-    else
-    {
-        outlet_list(x->f_out, s, argc, argv);
-    }
-}
-
-void prepend_anything(t_prepend *x, t_symbol *s, int argc, t_atom *argv)
-{
-    int i;
-    if(x->f_argc || x->f_selector != &s_list)
-    {
-        if(x->f_argc + argc + 1 > 255)
-        {
-            argc = 255 - x->f_argc - 1;
-        }
-        atom_setsym(x->f_argv, s);
-        for(i = x->f_argc; i < x->f_argc + argc; i++)
-        {
-            x->f_argv[i+1] = argv[i - x->f_argc];
-        }
-        outlet_anything(x->f_out, x->f_selector, x->f_argc + argc + 1, x->f_argv);
-    }
-    else if(x->f_selector != &s_list)
-    {
-        if(argc > 255)
-            argc = 255;
-        atom_setsym(x->f_argv, s);
-        for(i = 0; i < argc; i++)
-        {
-            x->f_argv[i+1] = argv[i];
-        }
-        outlet_anything(x->f_out, x->f_selector, argc+1, argv);
-    }
-    else
-    {
-        outlet_anything(x->f_out, s, argc, argv);
-    }
-}
-
-void prepend_float(t_prepend *x, float f)
-{
-    if(x->f_argc < 256)
-    {
-        atom_setfloat(x->f_argv+x->f_argc, f);
-        outlet_anything(x->f_out, x->f_selector, x->f_argc + 1, x->f_argv);
-    }
-    else
-        outlet_anything(x->f_out, x->f_selector, x->f_argc, x->f_argv);
-}
-
-void prepend_symbol(t_prepend *x, t_symbol *s)
-{
-    if(x->f_argc < 256)
-    {
-        atom_setsym(x->f_argv+x->f_argc, s);
-        outlet_anything(x->f_out, x->f_selector, x->f_argc + 1, x->f_argv);
-    }
-    else
-        outlet_anything(x->f_out, x->f_selector, x->f_argc, x->f_argv);
-}
-
-void prepend_bang(t_prepend *x)
-{
-    if(x->f_argc < 256)
-    {
-        atom_setsym(x->f_argv+x->f_argc, gensym("bang"));
-        outlet_anything(x->f_out, x->f_selector, x->f_argc + 1, x->f_argv);
-    }
-    else
-        outlet_anything(x->f_out, x->f_selector, x->f_argc, x->f_argv);
-}
-
-void prepend_free(t_prepend *x)
-{
-	eobj_free(x);
-}
-
-void prepend_assist(t_prepend *x, void *b, long m, long a, char *s)
-{
-	;
-}
 
 
