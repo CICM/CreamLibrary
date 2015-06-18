d85174f1a1d777943edc4373c0124d2f9881d86c
pierreguillot <guillotpierre6@gmail.com>
2015-06-18 16:51:41 +0200

    tests

--- c.library.cpp
+++ c.library.cpp
@@ -30,6 +30,49 @@ static void *cream_new(t_symbol *s)
     return (x);
 }
 
+/*
+static void epd_add_lib(const char* name)
+{
+    char path[MAXPDSTRING];
+    t_namelist* var = sys_searchpath;
+    while (var)
+    {
+        sprintf(path, "%s/%s",var->nl_string, name);
+        if(strncmp(var->nl_string, name, strlen(name)) == 0)
+        {
+            //post(var->nl_string);
+            sys_vgui((char *)"set creammenu .menubar.help\n $creammenu insert 3 command -label [_ \"Cream\"] -command {pdsend {pd open cream-help.pd %s}}\n", var->nl_string);
+            return;
+        }
+        else if(access(path, O_RDONLY) != -1)
+        {
+            //post(path);
+            sys_vgui((char *)"set creammenu .menubar.help\n $creammenu insert 3 command -label [_ \"Cream\"] -command {pdsend {pd open cream-help.pd %s}}\n", path);
+            return;
+        }
+        var = var->nl_next;
+    }
+    var = sys_staticpath;
+    while (var)
+    {
+        sprintf(path, "%s/%s",var->nl_string, name);
+        if(strncmp(var->nl_string, name, strlen(name)) == 0)
+        {
+            //post(var->nl_string);
+            sys_vgui((char *)"set creammenu .menubar.help\n $creammenu insert 3 command -label [_ \"Cream\"] -command {pdsend {pd open cream-help.pd %s}}\n", var->nl_string);
+            return;
+        }
+        else if(access(path, O_RDONLY) != -1)
+        {
+            //post(path);
+            sys_vgui((char *)"set creammenu .menubar.help\n $creammenu insert 3 command -label [_ \"Cream\"] -command {pdsend {pd open cream-help.pd %s}}\n", path);
+            return;
+        }
+        var = var->nl_next;
+    }
+}
+ */
+
 extern "C" void cream_setup(void)
 {
     cream_class = eclass_new("cream", (method)cream_new, (method)eobj_free, (short)sizeof(t_eobj), CLASS_PD, A_NULL, 0);
@@ -40,6 +83,7 @@ extern "C" void cream_setup(void)
         verbose(3, "Cream Library by Pierre Guillot\n© 2013 - 2015  CICM | Paris 8 University\nVersion %s (%s) for %s %i.%i\n",creamversion, __DATE__, pdversion, PD_MAJOR_VERSION, PD_MINOR_VERSION);
         eobj_free(obj);
     }
+    //epd_add_lib("cream");
     
     setup_c0x2ebang();
     setup_c0x2eblackboard();
